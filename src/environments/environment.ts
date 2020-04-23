3865137f4405cefa9dd24d3aa2bc81a80a2597ca
Lucas Micheli <lucasmicheli@outlook.com>
2020-04-23 16:21:43 -0300

    Upload all files of MVP.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  key: 'dcc928d056d6f0fd34d96818f924071a'
 };
 
 /*
